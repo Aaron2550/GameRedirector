dbf2e77b3e90c710debc3bf3d8d31098be118a9a
AaronVB <24834067+Aaron2550@users.noreply.github.com>
2023-05-18 14:15:45 +0200

    Code the Logic

--- Program.cs
+++ Program.cs
@@ -1,7 +1,46 @@
-﻿namespace GameRedirector;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
 
-internal class Program {
-	static void Main(string[] args) {
-		Console.WriteLine("Hello, World!");
+namespace GameRedirector;
+
+internal partial class Program {
+
+	[LibraryImport("kernel32.dll")]
+	private static partial IntPtr GetConsoleWindow();
+
+	[LibraryImport("user32.dll")]
+	[return: MarshalAs(UnmanagedType.Bool)]
+	private static partial bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+	static void Main(string[] Arguments) {
+		if (Arguments.Length == 0) {
+			Console.WriteLine("It seems like your Steam Command Line Arguments are not set up correctly. Please check and try again!");
+			Console.WriteLine("Set your Steam Arguments like this: '<Game Path> <Directory> <Arguments>'. The Directory and Arguments are optional.");
+			Console.WriteLine("Example: '\"D:\\SteamLibrary\\steamapps\\common\\GarrysMod\\hl2.exe\" \"D:\\SteamLibrary\\steamapps\\common\\GarrysMod\" \"-noworkshop\"'");
+			Console.WriteLine("Press Enter to exit.");
+			Console.ReadLine();
+			return;
+		}
+
+		string ExecutablePath = Arguments[0];
+		string Directory = Arguments.ElementAtOrDefault(1) ?? Path.GetDirectoryName(ExecutablePath)!;
+		string ArgumentString = string.Join(' ', Arguments.Skip(2));
+
+		Process GameProcess = new() {
+			StartInfo = {
+				FileName = ExecutablePath,
+				UseShellExecute = true,
+				WorkingDirectory = Directory,
+				Arguments = ArgumentString
+			}
+		};
+
+		Console.WriteLine($"Starting '{ExecutablePath}' in '{Directory}' with Arguments '{ArgumentString}'...");
+		GameProcess.Start();
+
+		Thread.Sleep(5000);
+		ShowWindow(GetConsoleWindow(), 0);
+
+		GameProcess.WaitForExit();
 	}
 }
